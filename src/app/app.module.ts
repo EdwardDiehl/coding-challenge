899f38e1ee157abcce5e47d054f816dadb82383b
Edward Diehl <EdwDiehl@gmail.com>
2017-02-21 19:20:49 +0200

    Add CodingChallengeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CodingChallengeComponent } from './coding-challenge/coding-challenge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodingChallengeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/coding-challenge/coding-challenge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'coding-challenge',
+  templateUrl: './coding-challenge.component.html',
+  styleUrls: ['./coding-challenge.component.scss']
+})
+export class CodingChallengeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
